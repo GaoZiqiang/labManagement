541403bcfbb07d915e0455549eac820e2ef73920
gaoziqiang <gaoziqianghi@163.com>
2017-07-14 22:07:39 +0800

    UploadRenderer.java

--- src/main/java/com/corejsf/UploadRenderer.java
+++ src/main/java/com/corejsf/UploadRenderer.java
@@ -19,11 +19,15 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.fileupload.FileItem;
+
+/**
+ * 将上传完的数据放在硬盘文件中或存储在值表达式中
+ */
 @Named("uploadRenderer")
 @FacesRenderer(componentFamily = "javax.faces.Input", rendererType = "com.corejsf.Upload")
 public class UploadRenderer extends Renderer {
 	/**
-	 * 呈现HTML元素
+	 * 呈现HTML元素，解析HTML封装
 	 */
 	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
 		System.out.println("断点测试:encodeBegin()方法");
@@ -48,8 +52,11 @@ public class UploadRenderer extends Renderer {
 		System.out.println("断点测试:decode()方法");
 		ExternalContext external = context.getExternalContext();
 		HttpServletRequest request = (HttpServletRequest) external.getRequest();
+		// 每次JSF请求的clientId
 		String clientId = component.getClientId(context);
+		// 将Servlet请求最终读取为一个临时文件夹保存到服务器的临时目录下
 		FileItem item = (FileItem) request.getAttribute(clientId);
+		System.out.println("打印FileItem:  " + item.toString());
 
 		Object newValue;
 		ValueExpression valueExpr = component.getValueExpression("value");
@@ -78,8 +85,12 @@ public class UploadRenderer extends Renderer {
 			((EditableValueHolder) component).setValid(true);
 		}
 
+		// 获取"target"
 		Object target = component.getAttributes().get("target");
 
+		//日志测试"target"
+		System.out.println("打印target:  " + target.toString());
+		// 解析"target"
 		if (target != null) {
 			File file;
 			if (target instanceof File)
@@ -88,14 +99,25 @@ public class UploadRenderer extends Renderer {
 				ServletContext servletContext = (ServletContext) external.getContext();
 				// 文件路径
 				String realPath = servletContext.getRealPath(target.toString());
-				// 打印输出路径
+				// realPath是文件上传到服务器所在的临时目录的路径
 				System.out.println("打印realPath:  " + realPath);
 				// 保存路径
 				file = new File(realPath);
+				file.getName();
+				file.getPath();
+				System.out.println("UploadRenderer.encode().file的name:  " + file.getName());
+				System.out.println("UploadRenderer.encode().file的path:  " + file.getPath());
+				// System.out.println("打印file: " + file.toString() +
+				// "打印filePath: " + file.toPath());
 			}
 
 			try { // ugh--write is declared with "throws Exception"
 				item.write(file);
+				item.getName();
+				// item.getRealPath();
+				System.out.println("打印item的fileName:  " + item.getName());
+				// System.out.println("打印item的fileRealPath: " +
+				// item.getRealPath());
 			} catch (Exception ex) {
 				throw new FacesException(ex);
 			}
